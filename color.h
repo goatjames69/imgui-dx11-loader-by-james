5f7dc17fd33946bb1b940faadb8e303800bee146
imjamesxd <129680954+jamesxdstore@users.noreply.github.com>
2023-05-05 16:24:05 +0545

    Add files via upload

---
+++ color.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "imgui.h"
+
+namespace colors {
+
+    inline ImColor lite_color = ImColor(40, 40, 40, 150);
+
+    inline ImVec4 Tab_Selected = ImColor(245, 250, 255, 255);
+    inline ImVec4 Tab_Hovered = ImColor(160, 170, 180, 100);
+    inline ImVec4 Tab = ImColor(240, 240, 240, 100);
+
+    inline ImVec4 Checkbox = ImColor(0, 0, 0, 100);
+    inline ImVec4 Checkbox_Hovered = ImColor(40, 45, 50, 150);
+    inline ImVec4 Checkbox_Active = ImColor(230, 235, 240, 255);
+
+    inline ImVec4 Slider = ImColor(40, 40, 40, 150);
+    inline ImVec4 Slider_Hovered = ImColor(40, 40, 40, 150);
+    inline ImVec4 Slider_Active = ImColor(100, 100, 100, 150);
+
+    inline ImVec4 Car_Slider = ImColor(150, 155, 160, 255);
+    inline ImVec4 Car_Slider_Hovered = ImColor(200, 210, 220, 255);
+    inline ImVec4 Car_Slider_Active = ImColor(230, 240, 250, 255);
+
+    inline ImVec4 Circle_Slider = ImColor(100, 100, 100, 255);
+    inline ImVec4 Circle_SliderHovered = ImColor(110, 110, 110, 255);
+    inline ImVec4 Circle_SliderActive = ImColor(130, 140, 150, 255);
+
+    inline ImVec4 Combo = ImColor(40, 45, 50, 180);
+    inline ImVec4 Combo_Hovered = ImColor(45, 55, 65, 180);
+    inline ImVec4 Combo_Active = ImColor(55, 55, 65, 180);
+
+    inline ImVec4 InputText = ImColor(40, 45, 50, 150);
+    inline ImVec4 InputText_Hovered = ImColor(45, 50, 55, 150);
+    inline ImVec4 InputText_Active = ImColor(55, 60, 65, 150);
+
+    inline ImVec4 Selectable_Hovered = ImColor(150, 150, 150, 255);
+    inline ImVec4 Selectable_Active = ImColor(250, 250, 250, 255);
+
+    inline ImVec4 Picker_Active = ImColor(30, 40, 50, 255);
+
+    inline ImVec4 Text = ImColor(150, 150, 150, 255);
+    inline ImVec4 Text_Hovered = ImColor(200, 200, 200, 255);
+    inline ImVec4 Text_Active = ImColor(235, 245, 255, 255);
+
+    inline ImVec4 CheckMark = ImColor(0, 0, 0, 255);
+
+    inline ImVec4 Transparent = ImColor(0, 0, 0, 0);
+
+    inline ImVec4 tab_tooltip = ImColor(180, 180, 180, 255);
+}
